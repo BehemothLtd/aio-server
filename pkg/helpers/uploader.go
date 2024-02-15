0fbcfb8a17000e0c0b13f3d763f254a3be5f3a62
bachdx2812 <bachdx.hut@gmail.com>
2024-02-15 17:06:57 +0700

    finish simple API upload file, return string

--- pkg/helpers/uploader.go
+++ pkg/helpers/uploader.go
@@ -2,15 +2,31 @@ package helpers
 
 import (
 	"aio-server/pkg/constants"
+	"context"
 	"errors"
+	"fmt"
+	"io"
 	"os"
+	"time"
+
+	"mime/multipart"
+
+	"cloud.google.com/go/storage"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Uploader struct {
-	Ctx *gin.Context
-	Local bool
+	Ctx        *gin.Context
+	Local      bool
+	UploadPath string
+}
+
+type ClientUploader struct {
+	cl         *storage.Client
+	projectID  string
+	bucketName string
+	uploadPath string
 }
 
 func (u *Uploader) Upload() (*string, error) {
@@ -26,10 +42,12 @@ func (u *Uploader) Upload() (*string, error) {
 		return nil, err
 	}
 
-	if u.Local {
-		uploadDst := os.Getenv("UPLOAD_LOCALLY_PATH") + NewUUID() +file.Filename
+	fileName := NewUUID() + file.Filename
 
-		err :=  u.Ctx.SaveUploadedFile(file, uploadDst)
+	if u.Local {
+		uploadDst := os.Getenv("UPLOAD_LOCALLY_PATH") + fileName
+
+		err := u.Ctx.SaveUploadedFile(file, uploadDst)
 
 		if err != nil {
 			return nil, err
@@ -37,8 +55,58 @@ func (u *Uploader) Upload() (*string, error) {
 
 		return &uploadDst, nil
 	} else {
-		print("UPLOAD TO LOCALLY \n")
-		// TODO: upload to GCS
-		return nil, nil
+		bucketName := os.Getenv("GCS_BUCKET_NAME")
+		projectId := os.Getenv("GCS_PROJECT_ID")
+		gcsAccountService := os.Getenv("GCS_ACCOUNT_SERVICE")
+
+		if bucketName == "" || projectId == "" || gcsAccountService == "" {
+			return nil, errors.New("Invalid Setting for Upload")
+		}
+
+		os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", gcsAccountService)
+		client, err := storage.NewClient(context.Background())
+		if err != nil {
+			return nil, err
+		}
+
+		uploadClient := &ClientUploader{
+			cl:         client,
+			bucketName: bucketName,
+			projectID:  projectId,
+			uploadPath: u.UploadPath,
+		}
+
+		blobFile, err := file.Open()
+		if err != nil {
+			return nil, err
+		}
+
+		err = uploadClient.UploadFile(blobFile, fileName)
+		if err != nil {
+
+			return nil, err
+		}
+
+		filePublicUrl := "https://storage.googleapis.com/" + bucketName + "/" + fileName
+
+		return &filePublicUrl, nil
 	}
+}
+
+func (c *ClientUploader) UploadFile(file multipart.File, object string) error {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+	wc := c.cl.Bucket(c.bucketName).Object(c.uploadPath + object).NewWriter(ctx)
+	if _, err := io.Copy(wc, file); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	return nil
 }
