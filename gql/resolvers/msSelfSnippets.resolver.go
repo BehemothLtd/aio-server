b9656bedd40841e5282fb16ad80dc65996b3aa17
bachdx2812 <bachdx.hut@gmail.com>
2024-02-21 01:43:02 +0700

    refine msSelfSnippets

---
+++ gql/resolvers/msSelfSnippets.resolver.go
@@ -0,0 +1,40 @@
+package resolvers
+
+import (
+	"aio-server/exceptions"
+	"aio-server/gql/gqlTypes/globalTypes"
+	"aio-server/gql/gqlTypes/msTypes"
+	"aio-server/gql/inputs"
+	"aio-server/models"
+	"aio-server/pkg/auths"
+	"aio-server/repository"
+	"context"
+)
+
+// MsSelfSnippets resolves the query for retrieving self-owned snippets.
+func (r *Resolver) MsSelfSnippets(ctx context.Context, args inputs.MsSnippetsInput) (*msTypes.MsSnippetsType, error) {
+	var snippets []*models.Snippet
+
+	user, err := auths.AuthUserFromCtx(ctx)
+
+	if err != nil {
+		return nil, exceptions.NewUnauthorizedError("")
+	}
+
+	snippetsQuery, paginationData := args.ToPaginationDataAndSnippetsQuery()
+
+	repo := repository.NewSnippetRepository(&ctx, r.Db)
+
+	fetchErr := repo.ListByUser(&snippets, &paginationData, &snippetsQuery, &user)
+
+	if fetchErr != nil {
+		return nil, exceptions.NewBadRequestError("")
+	}
+
+	return &msTypes.MsSnippetsType{
+		Collection: r.SnippetSliceToTypes(snippets),
+		Metadata: &globalTypes.MetadataType{
+			Metadata: &paginationData.Metadata,
+		},
+	}, nil
+}
--- gql/resolvers/snippets.queries.go
+++ gql/resolvers/snippets.queries.go
@@ -6,21 +6,6 @@ import (
 	"context"
 )
 
-// MsSelfSnippets resolves the query for retrieving self-owned snippets.
-func (r *Resolver) MsSelfSnippets(ctx context.Context, args inputs.MsSnippetsInput) (*payloads.MsSelfSnippetsResolver, error) {
-	resolver := payloads.MsSelfSnippetsResolver{
-		Ctx:  &ctx,
-		Db:   r.Db,
-		Args: args,
-	}
-
-	if err := resolver.Resolve(); err != nil {
-		return nil, err
-	}
-
-	return &resolver, nil
-}
-
 func (r *Resolver) MsSnippetDecryptContent(ctx context.Context, args inputs.MsSnippetDecryptContentInput) (*string, error) {
 	resolver := payloads.MsSnippetDecryptContentResolver{
 		Ctx:  &ctx,
