3a6abed41e73b08cf7ec82359401b8e16acc388a
bachdx2812 <bachdx.hut@gmail.com>
2024-02-21 00:55:55 +0700

    refine msSnippetFavorite

--- gql/inputs/msSnippetFavorite.input.go
+++ gql/inputs/msSnippetFavorite.input.go
@@ -2,6 +2,7 @@ package inputs
 
 import graphql "github.com/graph-gophers/graphql-go"
 
+// MsSnippetFavoriteInput represents args for toggle favorite on a snippet
 type MsSnippetFavoriteInput struct {
 	Id graphql.ID
 }
