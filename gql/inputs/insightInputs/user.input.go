3a696e00537551894ac804104d2cfa065829125f
behemoth-phucph <phucph@behemoth.vn>
2024-02-22 10:14:46 +0700

    User detal api

---
+++ gql/inputs/insightInputs/user.input.go
@@ -0,0 +1,7 @@
+package insightInputs
+
+import graphql "github.com/graph-gophers/graphql-go"
+
+type UserInput struct {
+	Id graphql.ID
+}
---
+++ gql/resolvers/insightResolvers/user.resolver.go
@@ -0,0 +1,38 @@
+package insightResolvers
+
+import (
+	"aio-server/exceptions"
+	"aio-server/gql/gqlTypes/globalTypes"
+	"aio-server/gql/inputs/insightInputs"
+	"aio-server/models"
+	"aio-server/pkg/helpers"
+	"aio-server/repository"
+	"context"
+
+	"gorm.io/gorm"
+)
+
+func (r *Resolver) User(ctx context.Context, args insightInputs.UserInput) (*globalTypes.UserType, error) {
+	if args.Id == "" {
+		return nil, exceptions.NewBadRequestError("Invalid Id")
+	}
+
+	userId, err := helpers.GqlIdToInt32(args.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user := models.User{}
+	repo := repository.NewUserRepository(&ctx, r.Db)
+	err = repo.FindById(&user, userId)
+
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, exceptions.NewRecordNotFoundError()
+		}
+		return nil, err
+	}
+
+	return &globalTypes.UserType{User: &user}, nil
+}
