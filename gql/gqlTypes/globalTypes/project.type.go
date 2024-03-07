bd0e5b37194ff2d576c2d3947a5a29ff7eb088bb
bachdx2812 <bachdx.hut@gmail.com>
2024-03-07 18:22:39 +0700

    init all types related to project

--- gql/gqlTypes/globalTypes/project.type.go
+++ gql/gqlTypes/globalTypes/project.type.go
@@ -27,3 +27,61 @@ func (tt *ProjectType) Name(ctx context.Context) *string {
 func (tt *ProjectType) Code(ctx context.Context) *string {
 	return &tt.Project.Code
 }
+
+func (tt *ProjectType) ProjectType(ctx context.Context) *string {
+	projectType := tt.Project.ProjectType.String()
+	return &projectType
+}
+
+func (tt *ProjectType) ProjectPriority(ctx context.Context) *string {
+	projectPriority := tt.Project.ProjectPriority.String()
+	return &projectPriority
+}
+
+func (tt *ProjectType) State(ctx context.Context) string {
+	return tt.Project.State.String()
+}
+
+func (tt *ProjectType) ActivedAt(ctx context.Context) *graphql.Time {
+	return helpers.GqlTimePointer(tt.Project.ActivedAt)
+}
+
+func (tt *ProjectType) InactivedAt(ctx context.Context) *graphql.Time {
+	return helpers.GqlTimePointer(tt.Project.InactivedAt)
+}
+
+func (tt *ProjectType) StartedAt(ctx context.Context) *graphql.Time {
+	return helpers.GqlTimePointer(tt.Project.StartedAt)
+}
+
+func (tt *ProjectType) EndedAt(ctx context.Context) *graphql.Time {
+	return helpers.GqlTimePointer(tt.Project.EndedAt)
+}
+
+func (tt *ProjectType) CreatedAt(ctx context.Context) *graphql.Time {
+	return helpers.GqlTimePointer(tt.Project.CreatedAt)
+}
+
+func (tt *ProjectType) UpdatedAt(ctx context.Context) *graphql.Time {
+	return helpers.GqlTimePointer(tt.Project.UpdatedAt)
+}
+
+func (tt *ProjectType) SprintDuration(ctx context.Context) *int32 {
+	return nil
+}
+
+func (pt *ProjectType) ClientId(ctx context.Context) *graphql.ID {
+	return helpers.GqlIDP(pt.Project.ClientId)
+}
+
+func (pt *ProjectType) CurrentSprintId(ctx context.Context) *graphql.ID {
+	return helpers.GqlIDP(pt.Project.CurrentSprintId)
+}
+
+func (pt *ProjectType) ProjectAssignees(ctx context.Context) *[]*ProjectAssigneeType {
+	return nil
+}
+
+func (pt *ProjectType) ProjectIssueStatuses(ctx context.Context) *[]*ProjectIssueStatusType {
+	return nil
+}
---
+++ gql/resolvers/insightResolvers/projectCreate.resolver.go
@@ -0,0 +1,11 @@
+package insightResolvers
+
+import (
+	"aio-server/gql/gqlTypes/globalTypes"
+	"aio-server/gql/inputs/insightInputs"
+	"context"
+)
+
+func (r *Resolver) ProjectCreate(ctx context.Context, args insightInputs.ProjectCreateInput) (*globalTypes.ProjectType, error) {
+	return nil, nil
+}
