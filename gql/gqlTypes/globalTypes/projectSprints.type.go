1ca1fac4ab9e679857a4833874109dd7222aec13
behemoth-phucph <phucph@behemoth.vn>
2024-03-10 16:43:04 +0700

    add api project_sprint

---
+++ gql/gqlTypes/globalTypes/projectSprints.type.go
@@ -0,0 +1,50 @@
+package globalTypes
+
+import (
+	"aio-server/models"
+	"aio-server/pkg/helpers"
+	"context"
+
+	graphql "github.com/graph-gophers/graphql-go"
+	"gorm.io/gorm"
+)
+
+type ProjectSprintType struct {
+	Ctx *context.Context
+	Db  *gorm.DB
+
+	ProjectSprint *models.ProjectSprint
+}
+
+func (pst *ProjectSprintType) ID(context.Context) *graphql.ID {
+	return helpers.GqlIDP(pst.ProjectSprint.Id)
+}
+
+func (pst *ProjectSprintType) Title(context.Context) string {
+	return pst.ProjectSprint.Title
+}
+
+func (pst *ProjectSprintType) ProjectId(context.Context) *graphql.ID {
+	return helpers.GqlIDP(pst.ProjectSprint.ProjectId)
+}
+
+func (pst *ProjectSprintType) StartDate(context.Context) *graphql.Time {
+	return helpers.GqlTimePointer(pst.ProjectSprint.StartDate)
+}
+
+func (pst *ProjectSprintType) EndDate(context.Context) *graphql.Time {
+	return helpers.GqlTimePointer(pst.ProjectSprint.EndDate)
+}
+
+// To do Archived
+
+func (pst *ProjectSprintType) UpdatedAt(context.Context) *graphql.Time {
+	return helpers.GqlTimePointer(pst.ProjectSprint.UpdatedAt)
+}
+func (pst *ProjectSprintType) CreatedAt(context.Context) *graphql.Time {
+	return helpers.GqlTimePointer(pst.ProjectSprint.CreatedAt)
+}
+
+func (pst *ProjectSprintType) lockVersion(context.Context) int32 {
+	return pst.ProjectSprint.LockVersion
+}
