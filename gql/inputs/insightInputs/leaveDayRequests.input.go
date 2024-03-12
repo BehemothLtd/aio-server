456b94bf16d97e01067d9224cf80b4c1a7b6e92d
behemoth-thangnt <thangnt@behemoth.vn>
2024-03-12 17:49:47 +0700

    "ldr input"

--- gql/inputs/insightInputs/leaveDayRequests.input.go
+++ gql/inputs/insightInputs/leaveDayRequests.input.go
@@ -2,9 +2,14 @@ package insightinputs
 
 import (
 	"aio-server/gql/inputs/globalInputs"
+	"aio-server/models"
 )
 
 type LeaveDayRequestsInput struct {
 	Input *globalInputs.PagyInput
 	Query *LeaveDayRequestsQueryInput
+}
+
+func (ldi *LeaveDayRequestsInput) ToPaginationDataAndQuery() (LeaveDayRequestsInput, models.PaginationData) {
+	paginationData := ldi.Input.ToPaginationInput()
 }
