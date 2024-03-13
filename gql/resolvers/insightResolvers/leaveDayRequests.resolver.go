4ee4bad30c61a918a3784d778d55e5f779bdc3e5
behemoth-thangnt <thangnt@behemoth.vn>
2024-03-13 19:13:02 +0700

    "ldrs resolver"

--- gql/resolvers/insightResolvers/leaveDayRequests.resolver.go
+++ gql/resolvers/insightResolvers/leaveDayRequests.resolver.go
@@ -1,11 +1,20 @@
 package insightresolvers
 
 import (
+	"aio-server/enums"
 	"aio-server/gql/gqlTypes/insightTypes"
 	"aio-server/gql/inputs/insightInputs"
+	"aio-server/models"
 	"context"
 )
 
 func (r *Resolver) LeaveDayRequests(ctx context.Context, args insightInputs.LeaveDayRequestsInput) (*insightTypes.LeaveDayRequestsType, error) {
+	if _, err := r.Authorize(ctx, enums.PermissionTargetTypeLeaveDayRequests.String(), enums.PermissionActionTypeRead.String()); err != nil {
+		return nil, err
+	}
 
+	var leaveDayRequests []*models.LeaveDayRequest
+	leaveDayRequestQuery, paginationData := args.ToPaginantionDataAndQuery()
+
+	// repo := repository
 }
---
+++ repository/leaveDayRequest.repo.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"aio-server/gql/inputs/insightInputs"
+	"aio-server/models"
+	"aio-server/pkg/helpers"
+	"context"
+
+	"gorm.io/gorm"
+)
+
+type LeaveDayRequestRepository struct {
+	Repository
+}
+
+func NewLeaveDayRequestRepository(c *context.Context, db *gorm.DB) *LeaveDayRequestRepository {
+	return &LeaveDayRequestRepository{
+		Repository: Repository{
+			db:  db,
+			ctx: c,
+		},
+	}
+}
+
+func (ldr *LeaveDayRequestRepository) List(
+	leaveDayRequests *[]*models.LeaveDayRequest,
+	paginationData *models.PaginationData,
+	query insightInputs.LeaveDayRequestsQueryInput,
+) error {
+	dbTables := ldr.db.Model(&models.LeaveDayRequest{})
+
+	return dbTables.Scopes(helpers.Paginate(dbTables.Scopes()))
+}
