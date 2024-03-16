62b6ea62a8cbd53e7f6634dc2e369eb1ad39ec0d
behemoth-thangnt <116883997+behemoth-thangnt@users.noreply.github.com>
2024-03-16 21:38:23 +0700

    Feature/leave day request show (#70)
    * "leave day request show"
    
    * "fix request preload user"

---
+++ gql/resolvers/insightResolvers/leaveDayRequest.resolver.go
@@ -0,0 +1,44 @@
+package insightResolvers
+
+import (
+	"aio-server/enums"
+	"aio-server/exceptions"
+	"aio-server/gql/gqlTypes/globalTypes"
+	"aio-server/gql/inputs/insightInputs"
+	"aio-server/models"
+	"aio-server/pkg/helpers"
+	"aio-server/repository"
+	"context"
+
+	"gorm.io/gorm"
+)
+
+func (r *Resolver) LeaveDayRequest(ctx context.Context, args insightInputs.LeaveDayRequestInput) (*globalTypes.LeaveDayRequestType, error) {
+	if _, err := r.Authorize(ctx, enums.PermissionTargetTypeLeaveDayRequests.String(), enums.PermissionActionTypeRead.String()); err != nil {
+		return nil, err
+	}
+
+	if args.Id == "" {
+		return nil, exceptions.NewBadRequestError("Invalid Id")
+	}
+
+	leaveDayRequestId, err := helpers.GqlIdToInt32(args.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	leaveDayRequest := models.LeaveDayRequest{}
+	repo := repository.NewLeaveDayRequestRepository(&ctx, r.Db.Preload("User").Preload("Approver"))
+	err = repo.FindById(&leaveDayRequest, leaveDayRequestId)
+
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, exceptions.NewRecordNotFoundError()
+		}
+
+		return nil, err
+	}
+
+	return &globalTypes.LeaveDayRequestType{LeaveDayRequest: &leaveDayRequest}, nil
+}
