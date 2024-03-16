f8260c11999ed45436530ac7ae9b25cbd3d08eea
hungtv <viethunglpt@gmail.com>
2024-03-16 16:02:51 +0700

    refactor: merge main

--- gql/resolvers/insightResolvers/concern.resolver.go
+++ gql/resolvers/insightResolvers/concern.resolver.go
@@ -50,6 +50,25 @@ func (r *Resolver) DeviceTypesSlideToType(deviceTypes []*models.DeviceType) *[]*
 	return &resolvers
 }
 
+func (r *Resolver) IssueStatusSliceToTypes(issueStatuses []*models.IssueStatus) *[]*globalTypes.IssueStatusType {
+	resolvers := make([]*globalTypes.IssueStatusType, len(issueStatuses))
+	for i, s := range issueStatuses {
+		resolvers[i] = &globalTypes.IssueStatusType{IssueStatus: s}
+	}
+
+	return &resolvers
+}
+
+func (r *Resolver) LeaveDayRequestSliceToTypes(requests []*models.LeaveDayRequest) *[]*globalTypes.LeaveDayRequestType {
+	resolvers := make([]*globalTypes.LeaveDayRequestType, len(requests))
+
+	for i, rq := range requests {
+		resolvers[i] = &globalTypes.LeaveDayRequestType{LeaveDayRequest: rq}
+	}
+
+	return &resolvers
+}
+
 // fromClients converts models.Client slice to []*ClientType.
 func (r *Resolver) ClientSliceToTypes(clients []*models.Client) *[]*globalTypes.ClientType {
 	resolvers := make([]*globalTypes.ClientType, len(clients))
