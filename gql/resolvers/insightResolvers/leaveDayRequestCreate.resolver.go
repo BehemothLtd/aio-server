fb4452d48c7e5909bc5214bd0606e0f3f681f80e
behemoth-thangnt <thangnt@behemoth.vn>
2024-03-29 18:18:33 +0700

    send message

--- gql/resolvers/insightResolvers/leaveDayRequestCreate.resolver.go
+++ gql/resolvers/insightResolvers/leaveDayRequestCreate.resolver.go
@@ -31,6 +31,8 @@ func (r *Resolver) LeaveDayRequestCreate(ctx context.Context, args insightInputs
 	if err := service.Excecute(); err != nil {
 		return nil, err
 	} else {
+		// Redis Job to send message to slack channel
+
 		return &insightTypes.LeaveDayRequestCreatedType{
 			LeaveDayRequest: &globalTypes.LeaveDayRequestType{
 				LeaveDayRequest: &request,
