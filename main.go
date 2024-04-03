178436a22beeabecb6e5862fb55001f063e03af4
bachdx2812 <bachdx.hut@gmail.com>
2024-04-03 16:04:00 +0700

    add async tasks

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 	"aio-server/pkg/auths"
 	"aio-server/pkg/initializers"
 	"aio-server/pkg/logger"
+	"aio-server/tasks"
+	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hibiken/asynq"
 	"github.com/sirupsen/logrus"
 )
 
@@ -16,6 +20,19 @@ func main() {
 
 	// Load DB
 	db := database.InitDb()
+
+	// Load Asynq
+	tasks.InitAsyncClient()
+
+	task, err := tasks.NewDemoTask(int32(1))
+	if err != nil {
+		log.Fatalf("could not create task: %v", err)
+	}
+	info, err := tasks.AsynqClient.Enqueue(task, asynq.ProcessIn(5*time.Second))
+	if err != nil {
+		log.Fatalf("could not enqueue task: %v", err)
+	}
+	log.Printf("enqueued task: id=%s queue=%s", info.ID, info.Queue)
 
 	r := gin.Default()
 
---
+++ tasks/asynq.go
@@ -0,0 +1,28 @@
+package tasks
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/hibiken/asynq"
+)
+
+const (
+	TypeDemoTask = "default:demo"
+)
+
+var AsynqClient *asynq.Client
+
+func InitAsyncClient() *asynq.Client {
+	if AsynqClient != nil {
+		return AsynqClient
+	}
+
+	redisAddr := os.Getenv("REDIS_URL")
+	fmt.Printf("REDIS ADDR : %+v", redisAddr)
+
+	AsynqClient = asynq.NewClient(asynq.RedisClientOpt{Addr: redisAddr})
+	// defer AsynqClient.Close()
+
+	return AsynqClient
+}
