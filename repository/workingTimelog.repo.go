620ac43d7d4983813dc7cbefa14e75e1a7d43c0e
behemoth-manhbd <manhbd@behemoth.vn>
2024-04-23 15:55:10 +0700

    fix fetch working logs

--- repository/workingTimelog.repo.go
+++ repository/workingTimelog.repo.go
@@ -34,6 +34,9 @@ func (wtr *WorkingTimelogRepository) FindById(workingTimeLog *models.WorkingTime
 func (wtr *WorkingTimelogRepository) List(workingTimeLogs *[]*models.WorkingTimelog, paginateData *models.PaginationData, query insightInputs.WorkingTimelogsQueryInput) error {
 	dbTables := wtr.db.Model(&models.WorkingTimelog{})
 
+	if query.IssueCodeEq != nil || query.IssueTitleCont != nil {
+		dbTables = dbTables.Joins("LEFT OUTER JOIN issues on working_timelogs.issue_id = issues.id")
+	}
 	return dbTables.Scopes(
 		helpers.Paginate(dbTables.Scopes(
 			wtr.DescriptionLike(query.DescriptionCont),
@@ -93,7 +96,7 @@ func (r *Repository) IssueTitleLike(issueTitleLike *string) func(db *gorm.DB) *g
 			return db
 		} else {
 			// return db.Where(gorm.Expr(`lower(issues.title) LIKE ?`, strings.ToLower("%"+*issueTitleLike+"%")))
-			return db.Joins("LEFT JOIN issues on lower(issues.title) LIKE = ?", strings.ToLower("%"+*issueTitleLike+"%"))
+			return db.Where(gorm.Expr(`lower(issues.title) LIKE ?`, strings.ToLower("%"+*issueTitleLike+"%")))
 		}
 	}
 }
@@ -103,7 +106,7 @@ func (r *Repository) IssueCodeEq(IssueCodeEq *string) func(db *gorm.DB) *gorm.DB
 		if IssueCodeEq == nil {
 			return db
 		} else {
-			return db.Joins("LEFT JOIN issues on issues.id = ?", IssueCodeEq)
+			return db.Where("issues.code = ?", IssueCodeEq)
 		}
 	}
 }
