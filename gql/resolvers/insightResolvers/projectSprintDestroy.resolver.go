7ceb32e64def26059f36e75ac0e5fb9f55e66675
bachdx2812 <bachdx.hut@gmail.com>
2024-05-14 10:07:31 +0700

    fix project sprint destroy api

--- gql/resolvers/insightResolvers/projectSprintDestroy.resolver.go
+++ gql/resolvers/insightResolvers/projectSprintDestroy.resolver.go
@@ -3,15 +3,19 @@ package insightResolvers
 import (
 	"aio-server/enums"
 	"aio-server/exceptions"
-	"aio-server/gql/inputs/insightInputs"
 	"aio-server/models"
 	"aio-server/pkg/helpers"
 	"aio-server/repository"
 	"context"
 	"fmt"
+
+	"github.com/graph-gophers/graphql-go"
 )
 
-func (r *Resolver) ProjectSprintDestroy(ctx context.Context, args insightInputs.ProjectSprintInput) (*string, error) {
+func (r *Resolver) ProjectSprintDestroy(ctx context.Context, args struct {
+	ProjectId graphql.ID
+	Id        graphql.ID
+}) (*string, error) {
 	if _, err := r.Authorize(ctx, enums.PermissionTargetTypeProjectSprints.String(), enums.PermissionActionTypeDelete.String()); err != nil {
 		return nil, err
 	}
@@ -22,8 +26,20 @@ func (r *Resolver) ProjectSprintDestroy(ctx context.Context, args insightInputs.
 		return nil, exceptions.NewBadRequestError("Invalid ID")
 	}
 
+	projectId, err := helpers.GqlIdToInt32(args.ProjectId)
+	if err != nil || projectId == 0 {
+		return nil, exceptions.NewBadRequestError("Invalid Id")
+	}
+
+	project := models.Project{Id: projectId}
+	projectRepo := repository.NewProjectRepository(&ctx, r.Db)
+	if err := projectRepo.Find(&project); err != nil {
+		return nil, exceptions.NewBadRequestError("Invalid Project")
+	}
+
 	projectSprint := models.ProjectSprint{
-		Id: projectSprintId,
+		Id:        projectSprintId,
+		ProjectId: projectId,
 	}
 
 	repo := repository.NewProjectSprintRepository(&ctx, r.Db.Preload("Project").Preload("Issues"))
